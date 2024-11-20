acec230bbca521a912a257527da8ff6f03d4a9f5
Pakorn Soikham <109463911+newpkorn@users.noreply.github.com>
2024-11-20 11:50:18 +0700

    Update routers.js

--- router/routers.js
+++ router/routers.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-// const multer = require('multer');
+const multer = require('multer');
 
 // Middleware.
 const authMiddleware = require('../middleware/authMiddleware');
